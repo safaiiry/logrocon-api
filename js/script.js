0d56e80431e02035d75feb32441f7442f134b8b2
safaiiry <arina.safronova.viantec@gmail.com>
2024-07-12 13:46:37 +0300

    added a loader when loading a card, removed page scrolling when the card is open

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ let ingredientsContainer = document.querySelector(".ingredients-content");
 let directionsContainer = document.querySelector(".directions-content");
 const loadMoreBtn = document.getElementById('load-more-btn');
 const modal = document.querySelector(".modal-overlay");
+const modalWindow = document.querySelector(".modal-wrapper");
 const closeBtn = document.querySelector(".close-modal-btn");
+
 
 let name;
 let startingIndex = 0;
@@ -13,6 +15,8 @@ let recipeDataArr = [];
 let recipeModalArr = [];
 let directions = [];
 let ingredients = [];
+
+let mask = document.querySelector('.mask');
 
 fetch('https://jellybellywikiapi.onrender.com/api/Recipes') 
     .then((res) => res.json())
@@ -47,13 +51,21 @@ fetch('https://jellybellywikiapi.onrender.com/api/Recipes?pageIndex=3')
 
 
 const getDataById = (id) => {
+    var isLoad = false;
+    mask.classList.remove("hide");
+    modalWindow.classList.add("hide");
     fetch(`https://jellybellywikiapi.onrender.com/api/Recipes/${id}`)
     .then((res) => res.json())
     .then((data) => {
         recipeModalArr = data;
         directions = data.directions;
         ingredients = data.ingredients;
-        displayRecipeById(ingredients, directions)
+        isLoad = true;
+        if (isLoad) {
+            mask.classList.add("hide");
+            modalWindow.classList.remove("hide");
+            displayRecipeById(ingredients, directions);
+        }
       })
     .catch(
         (err) => console.error(`There was an error: ${err}`));
@@ -102,10 +114,12 @@ const fetchMoreRecipes = () => {
 function openModal(id) {
     modal.classList.remove("hide");
     getDataById(id);
-
+    document.body.style.overflow = 'hidden';
+    
 }
 
 function closeModal(e, clickedOutside) {
+    document.body.style.overflow = '';
     if (clickedOutside) {
         if (e.target.classList.contains("modal-overlay"))
             modal.classList.add("hide");
@@ -127,5 +141,8 @@ function cleanModal() {
 
 }
 
+
+
 closeBtn.addEventListener("click", closeModal);
 loadMoreBtn.addEventListener('click', fetchMoreRecipes);
+
